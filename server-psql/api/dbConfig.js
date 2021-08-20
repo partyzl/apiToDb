def99a2e3b495b811be7f3e0cb199a1f3b271d7a
Parth Patel <parth6796@yahoo.com>
2021-08-20 14:12:00 +0100

    mariusz is the best

--- server-psql/api/dbConfig.js
+++ server-psql/api/dbConfig.js
@@ -1,11 +1,5 @@
 const {Pool} = require('pg');
 
-const pool = new Pool({
-    host: 'db',
-    user: 'phonebook',
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-});
+const pool = new Pool({database: process.env.PGDATABASE});
 
 module.exports = pool;
--- server-psql/api/index.js
+++ server-psql/api/index.js
@@ -1,7 +1,5 @@
 const app = require('./server');
-const { Pool } = require('pg');
 
 const port = process.env.PORT || 3000;
-const pool = new Pool();
 
 app.listen(port, ()=>console.log(`Express is departing at ${port}`));
